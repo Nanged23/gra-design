6696cb762dd37c29478d41af944f3ecbf0d86cec
Nanged23 <2074759416@qq.com>
2024-12-31 11:35:06 +0800

    ds

--- front-end/src/components/statistic/statistics.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div>这里是数据统计区</div>
-</template>
--- front-end/src/components/write/article.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="content">
-        <div>这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article 这是 article </div>
-        
-    </div>
-</template>
-<style>
- 
-</style>
