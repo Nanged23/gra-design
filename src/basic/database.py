fa4e021329b460a828ac9c61758c51895ce111fa
Nanged23 <2074759416@qq.com>
2025-02-13 16:05:53 +0800

    6

--- src/basic/database.py
+++ src/basic/database.py
@@ -11,17 +11,5 @@ from dotenv import load_dotenv
 load_dotenv()
 # MySQL 的 ORM
 db = SQLAlchemy()
-
-# Redis 连接
-parsed_url = urlparse(os.getenv('REDIS_REMOTE_URL'))
-
-pool = redis.ConnectionPool(
-    host=parsed_url.hostname,
-    port=parsed_url.port,
-    password=parsed_url.password,
-    db=0,
-    decode_responses=True,
-)
-
 # 创建 Redis 客户端实例
-redis_client = redis.Redis(connection_pool=pool)
+redis_client = redis.from_url(os.getenv("REDIS_INNER_URL"))
